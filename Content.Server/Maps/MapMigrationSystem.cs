526b2d19e97b435ddb994f12e85a6ad6d98d0d9b
Gerkada <60323761+Gerkada@users.noreply.github.com>
2026-01-01 16:55:19 +0200

    Fix migrations (#126)
    * fix migrations
    
    * erm
    
    * chore: Automatically update REUSE headers
    
    ---------
    
    Co-authored-by: github_actions[bot] <github-actions[bot]@users.noreply.github.com>

--- Content.Server/Maps/MapMigrationSystem.cs
+++ Content.Server/Maps/MapMigrationSystem.cs
@@ -1,4 +1,13 @@
-﻿using System.Diagnostics.CodeAnalysis;
+// SPDX-FileCopyrightText: 2024 ElectroJr
+// SPDX-FileCopyrightText: 2025 B_Kirill
+// SPDX-FileCopyrightText: 2025 Kyle Tyo
+// SPDX-FileCopyrightText: 2025 LaCumbiaDelCoronavirus
+// SPDX-FileCopyrightText: 2025 Leon Friedrich
+// SPDX-FileCopyrightText: 2026 Gerkada
+//
+// SPDX-License-Identifier: MIT
+
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 using Robust.Shared.ContentPack;
@@ -23,7 +32,7 @@ public sealed class MapMigrationSystem : EntitySystem
     [Dependency] private readonly IResourceManager _resMan = default!;
 
     private const string MigrationFile = "/migration.yml";
-    private const string MigrationFileKs = "/migration.yml_ks14"; // KS14
+    private const string MigrationFileKs14 = "/migration_ks14.yml"; // KS14
 
     public override void Initialize()
     {
@@ -42,7 +51,7 @@ public sealed class MapMigrationSystem : EntitySystem
                 DebugTools.Assert(_protoMan.HasIndex<EntityPrototype>(newId), $"{newId} is not an entity prototype.");
         }
 
-        if (!TryReadFile(MigrationFileKs, out var mappingsKs))
+        if (!TryReadFile(MigrationFileKs14, out var mappingsKs)) // KS14
             return;
 
         // Verify that all of the entries map to valid entity prototypes.
@@ -92,6 +101,6 @@ public sealed class MapMigrationSystem : EntitySystem
     private void OnBeforeReadEvent(BeforeEntityReadEvent ev)
     {
         ProcessEvent(ref ev, MigrationFile);
-        ProcessEvent(ref ev, MigrationFileKs);
+        ProcessEvent(ref ev, MigrationFileKs14); // KS14
     }
 }
